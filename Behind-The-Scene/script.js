6bbd016a014ba97c6452ee873fea0487c4064d4f
Vidhathri Bhat <vidhathribhat24@gmail.com>
2025-02-05 21:36:45 +0530

    code added

--- Behind-The-Scene/script.js
+++ Behind-The-Scene/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 // function calcAge(birthYeah) {
 //     const age = 2037 - birthYeah;
@@ -37,136 +37,234 @@
 
 // // printAge();
 
+//////////////////////////////////////////////////////////
 
-//--------------------------------------------------------
-// // The this Keyword in Practice
-// console.log(this);
+// //Variables
+// console.log(me);
+// console.log(job);   //Uncaught ReferenceError: Cannot access 'job' before initialization
+// console.log(year);
 
-// const calcAge = function (birthYear) {
-//   console.log(2037 - birthYear);
-//   console.log(this);
-// };
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+//Functions
+
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//     return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//     return a + b;
+// }
+
+// var addArrow = (a, b) => a + b;
+
+//Example
+
+/*if (!numProducts) deleteShoppingCart(); //here the output is 'All products deleted' coz at this particular line the value is not u=yet  given
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log(`All products deleted!`);
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);    //variables declared with var creates a property on the global window object ,check at window(@ console)
+console.log(y === window.y);
+console.log(z === window.z);*/
+
+//////////////////////////////
+
+// console.log(this);      //global scope - window object
+
+// const calcAge = function (birthYeah) {
+//     console.log(2037 - birthYeah);
+//     console.log(this);  //didnt attach to any object / simply no owner in this case
+// }
 // calcAge(1991);
 
-// const calcAgeArrow = birthYear => {
-//   console.log(2037 - birthYear);
-//   console.log(this);
-// };
-// calcAgeArrow(1980);
+// const calcAgeArrow = (birthYeah) => {
+//     console.log(2037 - birthYeah);
+//     console.log(this);  //uses parent's scope i.e windows
+// }
+// calcAgeArrow(1991);
 
+/*const jonas = {
+    year: 1991,
+    calcAge: function () {
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+};
+jonas.calcAge();
+
+const nutella = {
+    year: 2017,
+};
+nutella.calcAge = jonas.calcAge;
+nutella.calcAge();
+
+const f = jonas.calcAge;
+f();    //error occurs because f() is calling the function jonas.calcAge without a proper this context.*/
+
+/*const jonas = {
+    year: 1991,
+    firstName: 'Jonas',
+    calcAge: function () {
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+    greet: () => {
+        console.log(`Hey! ${this.firstName}`);  //arrow uses parent's scope i.e global scope here
+    },
+};
+jonas.greet();
+console.log(this.firstName);*/
+
+//Var shouldn't be used here
+
+// var firstName = 'Matilla';
 // const jonas = {
-//   year: 1991,
-//   calcAge: function () {
-//     console.log(this);
-//     console.log(2037 - this.year);
-//   },
+//     year: 1991,
+//     firstName: 'Jonas',
+//     calcAge: function () {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//     },
+//     greet: () => {
+//         console.log(this);
+//         console.log(`Hey! ${this.firstName}`);  //arrow uses parent's scope i.e Matilla by var
+//     },
 // };
-// jonas.calcAge();
+// jonas.greet();
+// console.log(this.firstName);
 
-// const matilda = {
-//   year: 2017,
-// };
-
-// matilda.calcAge = jonas.calcAge;
-// matilda.calcAge();
-
-// const f = jonas.calcAge;
-// f();
-
-
-// ///////////////////////////////////////
-// // Regular Functions vs. Arrow Functions
-// // var firstName = 'Matilda';
-
+/*
 // const jonas = {
-//   firstName: 'Jonas',
-//   year: 1991,
-//   calcAge: function () {
-//     // console.log(this);
-//     console.log(2037 - this.year);
+//     year: 1991,
+//     firstName: 'Jonas',
+//     calcAge: function () {
+//         console.log(this);
+//         console.log(2037 - this.year);
 
-//     // Solution 1
-//     // const self = this; // self or that
-//     // const isMillenial = function () {
-//     //   console.log(self);
-//     //   console.log(self.year >= 1981 && self.year <= 1996);
-//     // };
-
-//     // Solution 2
-//     const isMillenial = () => {
-//       console.log(this);
-//       console.log(this.year >= 1981 && this.year <= 1996);
-//     };
-//     isMillenial();
-//   },
-
-//   greet: () => {
-//     console.log(this);
-//     console.log(`Hey ${this.firstName}`);
-//   },
+//         const isMillenial = function () {
+//             console.log(this);  //ERROR: 'this' is undefined
+//             //isMillenial() is a regular function call (not a method of jonas).
+//             //Solution: Use an Arrow Function
+//             console.log(this.year >= 1981 && this.year <= 1996);
+//         };
+//         isMillenial();
+//     },
+//     greet: () => {
+//         console.log(`Hey! ${this.firstName}`);  //arrow uses parent's scope i.e global scope here
+//     },
 // };
 // jonas.greet();
 // jonas.calcAge();
 
-// // arguments keyword
-// const addExpr = function (a, b) {
-//   console.log(arguments);
-//   return a + b;
-// };
-// addExpr(2, 5);
-// addExpr(2, 5, 8, 12);
+//Solution
+const jonas = {
+    year: 1991,
+    firstName: 'Jonas',
+    calcAge: function () {
+        console.log(this);
+        console.log(2037 - this.year);
 
-// var addArrow = (a, b) => {
-//   console.log(arguments);
-//   return a + b;
-// };
-// addArrow(2, 5, 8);
+        //Solution 1
+        // const self = this;  //self or that workaround
+
+        // const isMillenial = function () {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+        // isMillenial();
+
+        //Solution 2
+        const isMillenial = () => {
+            console.log(self);
+            console.log(self.year >= 1981 && self.year <= 1996);
+        };
+        isMillenial();
+    },
+    greet: () => {
+        console.log(`Hey! ${this.firstName}`);  //arrow uses parent's scope i.e global scope here
+    },
+};
+jonas.greet();
+jonas.calcAge();*/
 
 
-// ///////////////////////////////////////
-// // Object References in Practice (Shallow vs. Deep Copies)
+/*const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
+}
+addExpr(2, 2);
+console.log(addExpr(2, 3, 4, 5, 5));
 
-// const jessica1 = {
-//   firstName: 'Jessica',
-//   lastName: 'Williams',
-//   age: 27,
-// };
+var addArrow = (a, b) => {
+    console.log(arguments); //gives error
+    //Arrow functions do NOT have their own arguments object.
+    a + b
+};
+addArrow(2, 3, 4);*/
 
-// function marryPerson(originalPerson, newLastName) {
-//   originalPerson.lastName = newLastName;
-//   return originalPerson;
-// }
+/*//primitives vs objects(reference types)
 
-// const marriedJessica = marryPerson(jessica1, 'Davis');
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
 
-// // const marriedJessica = jessica1;
-// // marriedJessica.lastName = 'Davis';
+const me = {
+    name: 'Jonas',
+    age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend', friend);
+console.log('me', me);*/
 
-// console.log('Before:', jessica1);
-// console.log('After:', marriedJessica);
+/*//Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
 
-// const jessica = {
-//   firstName: 'Jessica',
-//   lastName: 'Williams',
-//   age: 27,
-//   familiy: ['Alice', 'Bob'],
-// };
+//Reference types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage', jessica);
+console.log('After marriage', marriedJessica);
+//Both gives the same output coz,the marriedJessica dont create a new object in the heap
 
-// // Shallow copy
-// const jessicaCopy = { ...jessica };
-// jessicaCopy.lastName = 'Davis';
+//Copying object
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Barney'],
+};
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage', jessica2);
+console.log('After marriage', jessicaCopy);
 
-// // jessicaCopy.familiy.push('Mary');
-// // jessicaCopy.familiy.push('John');
-
-// // console.log('Before:', jessica);
-// // console.log('After:', jessicaCopy);
-
-// // Deep copy/clone
-// const jessicaClone = structuredClone(jessica);
-// jessicaClone.familiy.push('Mary');
-// jessicaClone.familiy.push('John');
-
-// console.log('Original:', jessica);
-// console.log('Clone:', jessicaClone);
-// */
+jessicaCopy.family.push('Robin');
+jessicaCopy.family.push('Ted Mosby');
+//Here the lastname was changed but family(array) are same in both,it is changed even in the first
+//So we do the deepClone (using external libraries)*/
